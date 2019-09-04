0e909f892d1e6357dd4408d628d0704c7a4d87bc
2514939946@qq.com <2514939946@qq.com>
2019-09-04 14:26:35 +0800

    修改提交

--- src/page/home/index.js
+++ src/page/home/index.js
@@ -1,7 +1,31 @@
 import React from 'react'
-const Home = () =>{
-  return(
-    <h1>我是首页</h1>
-  )
+import { Route } from 'react-router-dom'
+import HomeList from '../HomeList'
+import News from '../News'
+import Index from '../Index'
+import Profile from '../Profile'
+// 导入 antd 组件
+import { NavBar, Icon } from 'antd-mobile';
+export default class Home extends React.Component{
+  render(){
+    return(
+        <div>
+          <Route path="/home/index" component={Index} />
+          <Route path="/home/list" component={HomeList} />
+          <Route path="/home/news" component={News} />
+          <Route path="/home/profile" component={Profile} />
+          <div>
+            <NavBar
+                mode="light"
+                icon={<Icon type="left" />}
+                onLeftClick={() => console.log('onLeftClick')}
+                rightContent={[
+                  <Icon key="0" type="search" style={{ marginRight: '16px' }} />,
+                  <Icon key="1" type="ellipsis" />,
+                ]}
+            >NavBar</NavBar>
+          </div>
+        </div>
+    )
+  }
 }
-export default Home
